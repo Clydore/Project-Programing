9f7ed313594edf648f2efa46f8b1928a9cde22cc
Trevor <clydore@gmail.com>
2016-12-01 20:54:48 -0700

    Some more uncle scrooge

--- MainProject.py
+++ MainProject.py
@@ -1,9 +1,112 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def MeetsUncleScrooge:
     print """
     You are laying in bed morning the loss of your parents and you can hear the
-    slow drops of rain on your roof...
+    slow drops of rain on the roof. The constant pitter patter of methodic rain...
     """
     raw_input()
     print """
-    Suddenly there is a knock on the door
+    Suddenly there is a knock on the door you get up groggily to answer it. A
+    man with a scraggly unkept beard stares at you with a crazy glint in his eye
+    says, "Hey I heard about your loss..." You suddenly realize he has one sword
+    in his sheath by his hip and a sword in his opposing hand. "Here take this.
+    Your gonna need it." Do you take the sword or refuse?
     """
